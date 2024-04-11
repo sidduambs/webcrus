95db8b0cf837530b74096528502f0b04941300c0
Siddu Ambesange <sidduambesange005@gmail.com>
2024-04-11 07:40:11 +0530

    updatedddddddd

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
     }
   },
   esbuild: {
-    jsxFactory: 'createRoot' // Correct JSX factory function
+    jsxFactory: createRoot // Correct JSX factory function
   }
 });
